64bd1f36ee6a4730f0e4941f800cd52f351432e5
Onsahmed <ons.ahmed@esprit.tn>
2024-04-01 00:19:35 +0100

    front+back

---
+++ src/Controller/CoursController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Cours;
+use App\Form\CoursType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/cours')]
+class CoursController extends AbstractController
+{
+    #[Route('/', name: 'app_cours_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $cours = $entityManager
+            ->getRepository(Cours::class)
+            ->findAll();
+
+        return $this->render('cours/index.html.twig', [
+            'cours' => $cours,
+        ]);
+    }
+
+    #[Route('/coursfront', name: 'app_cours')]
+    public function cours(EntityManagerInterface $entityManager): Response
+    {
+        $cours = $entityManager
+            ->getRepository(Cours::class)
+            ->findAll();
+        return $this->render('Front/cours.html.twig', [
+            'cours' => $cours,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_cours_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $cour = new Cours();
+        $form = $this->createForm(CoursType::class, $cour);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($cour);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_cours_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('cours/new.html.twig', [
+            'cour' => $cour,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_cours_show', methods: ['GET'])]
+    public function show(Cours $cour): Response
+    {
+        return $this->render('cours/show.html.twig', [
+            'cour' => $cour,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_cours_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Cours $cour, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CoursType::class, $cour);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_cours_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('cours/edit.html.twig', [
+            'cours' => $cour,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_cours_delete', methods: ['POST'])]
+    public function delete(Request $request, Cours $cour, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$cour->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($cour);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_cours_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/CoursType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cours;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CoursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('contenu')
+            ->add('etat')
+            ->add('rate')
+            ->add('idCat')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Cours::class,
+        ]);
+    }
+}
